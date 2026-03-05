c7fb15798dd94d4445ba9c17000ed66d60972689
Prutha Dongre <pruthadongre09@gmail.com>
2026-03-05 22:12:32 +0530

    Update App.java

--- App.java
+++ App.java
@@ -2,8 +2,13 @@ package com.example;
 
 public class App {
 
-    public static void main(String[] args) {
-        System.out.println("Java Maven Application running inside Docker!");
-    }
+    public static void main(String[] args) throws Exception {
 
+        System.out.println("Java Maven Application running inside Docker!");
+
+        while(true){
+            Thread.sleep(10000);
+        }
+
+    }
 }
